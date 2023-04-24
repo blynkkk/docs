b87aee9069f16b9b37098bd0f25bac745138a8a9
Blynk IoT platform <pavel@blynk.cc>
2023-04-24 17:40:58 -0400

    Update main.ino

--- unused/main.ino
+++ unused/main.ino
@@ -21,7 +21,7 @@ int alertStatus = 0;
 
 void myTimerEvent()
 {
-  int value = random (0, 100);
+  int value = random(0, 100);
   alertStatus = map(value, 0, 100, 0, 2);
   Blynk.virtualWrite(V2, value);
 
@@ -29,22 +29,18 @@ void myTimerEvent()
   {
   case 0:
     Blynk.setProperty(V0, "url", "https://raw.githubusercontent.com/blynkkk/docs/main/unused/astronaut.json");
-    Blynk.setProperty(V0, "loop", "true");
     break;
 
   case 1:
     Blynk.setProperty(V0, "url", "https://raw.githubusercontent.com/blynkkk/docs/main/unused/orangealert.json");
-    Blynk.setProperty(V0, "loop", "true");
     break;
 
   case 2:
     Blynk.setProperty(V0, "url", "https://raw.githubusercontent.com/blynkkk/docs/main/unused/redalert.json");
-    Blynk.setProperty(V0, "loop", "true");
     break;
 
   default:
     Blynk.setProperty(V0, "url", "https://raw.githubusercontent.com/blynkkk/docs/main/unused/astronaut.json");
-    Blynk.setProperty(V0, "loop", "true");
     break;
   }
 }
@@ -54,7 +50,7 @@ void setup()
   // Debug console
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, BLYNK_SERVER);
-  timer.setInterval(5000L, myTimerEvent);
+  timer.setInterval(10000L, myTimerEvent);
 }
 
 void loop()
